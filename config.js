5a79e743fbda51caaaa8b4f163493338059d5f85
gmnnimsara <nethmintha@gmail.com>
2025-02-11 13:03:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,9 @@ let sensorNomor = true; // ubah jadi false jika tidak ingin menyensor nomor yang
 let blackList = ["628987654321", "628123456789"]; // nomor yang ada di dalam array ini tidak akan dilihat statusnya
 let whiteList = []; // jika array ini tidak kosong, hanya nomor yang ada di dalam array ini yang akan dilihat statusnya
 
-module.exports = { autoLikeStatus, downloadMediaStatus, sensorNomor, blackList, whiteList };
+
+module.exports = { 
+    
+    SESSION_ID: process.env.SESSION_ID || "vMFWzAJT#SUL477kcgy4h3BJtKepQA75oCp9ombC3hdNfZZNlN-w",
+    
+    autoLikeStatus, downloadMediaStatus, sensorNomor, blackList, whiteList };
